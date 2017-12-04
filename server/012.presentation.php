9c503b45f0412952a4581241c8879912e31d81ec
Thibault <thibault@gipilab.org>
2017-12-04 15:42:25 +0100

    Updated presentation

--- server/012.presentation.php
+++ server/012.presentation.php
@@ -50,18 +50,18 @@
   <div class="row">
     <div class="col l6 s12">
       <h3>Présentation</h3>
-      <p>L'observatoire des taux se propose de guider la réflexion des décideurs politiques et administratifs vers les choix les mieux adaptés à leurs besoins, dans les domaines sensibles de la maîtrise du coût de la dette et de la gestion des risques environnant un encours.</p>
-      <p><strong>A travers cet outil, le Laboratoire de Recherche pour le Développement Local prend ouvertement position en faveur de l'indice EURIBOR pour l'adossement des emprunts à souscrire par les Collectivités locales.</strong></p>
-      <p>Les supports d'adossement du type TEC pour les emprunts dits «&nbsp;à taux fixe&nbsp;» sont déconseillés par le Laboratoire du fait des risques majeurs que ces emprunts font peser sur les Collectivités emprunteuses et dont il est essentiel de prendre la mesure. Ces risques sont qualifiés d'indirects par le Laboratoire car une apparente innocuité des taux fixes est entretenue par les argumentaires de vente.</p>
+      <p>L'observatoire des taux se propose de guider votre réflexion vers les choix les mieux adaptés à vos besoins dans les domaines sensibles de la maîtrise du coût des emprunts.</p>
+      <p><strong>A travers cet outil, le Laboratoire de Recherche pour le Développement Local prend ouvertement position en faveur de l'indice EURIBOR pour l'adossement des emprunts à souscrire</strong></p>
+      <p>Les supports d'adossement du type TEC pour les emprunts dits «&nbsp;à taux fixe&nbsp;» sont déconseillés par le Laboratoire du fait des risques majeurs que ces emprunts font peser sur les emprunteurs et dont il est essentiel de prendre la mesure. Ces risques sont qualifiés d'indirects par le Laboratoire car une apparente innocuité des taux fixes est entretenue par les argumentaires de vente.</p>
       <p>Cette position est renforcée par la théorie dénommée «&nbsp;<b>GFM©</b>&nbsp;» présentée par le Laboratoire de Recherche pour le Développement Local sous la forme d'une démonstration graphique du risque de surcoût qu'induit la souscription d'un emprunt à taux fixe sur référence TEC ou tout autre indice.</p>
     </div>
     <div class="col l6 s12">
       <h4>Ironie du verbe ?</h4>
-      <p>Pour une Collectivité territoriale, l'unique justification d'un taux fixe, soit la garantie de stabilité face à une remontée (en cours ou hypothétique) des taux indexés, est largement débordée lorsque la conjoncture vient à tirer les taux indexés vers le bas ! Le taux fixe joue alors, en effet, un rôle inverse à celui pour lequel il a été choisi : blocage des décisions à cause de l'impact financier de l'indemnité actuarielle, surcoût des échéances, critiques du poids budgétaire de la dette...<br>
+      <p>Pour un emprunteur, l'unique justification d'un taux fixe, soit la garantie de stabilité face à une remontée (en cours ou hypothétique) des taux indexés, est largement débordée lorsque la conjoncture vient à tirer les taux indexés vers le bas ! Le taux fixe joue alors, en effet, un rôle inverse à celui pour lequel il a été choisi : blocage des décisions à cause de l'impact financier de l'indemnité actuarielle, surcoût des échéances, critiques du poids budgétaire de la dette...<br>
       Or, il s'avère qu'il est dans la nature même des taux indexés de monter, de descendre, puis de monter à nouveau, et ainsi de suite.</p>
       <p><strong>Il s'agit d'une oscillation, liée à divers paramètres politiques, économiques et financiers, nationaux et internationaux.</strong></p>
-      <p>Les marchés inspirent et expirent. Lorsqu'ils sont bas, il advient nécessairement que les taux indexés remontent, simplement pour qu'ils puissent redescendre ensuite, dès que possible (ou que nécessaire). L'histoire démontre également que la période de cette oscillation est toujours inférieure de plusieurs grandeurs à la durée (15 ans, parfois plus) des emprunts souscrits par les Collectivités territoriales.</p>
-      <p><strong>Il est donc primordial que les décideurs locaux disposent, avec l'Observatoire des taux, des éléments d'analyses qui leur permettent de prendre en compte cet état de fait.</strong></p>
+      <p>Les marchés inspirent et expirent. Lorsqu'ils sont bas, il advient nécessairement que les taux indexés remontent, simplement pour qu'ils puissent redescendre ensuite, dès que possible (ou que nécessaire). L'histoire démontre également que la période de cette oscillation est toujours inférieure de plusieurs grandeurs à la durée (15 ans, parfois plus) des emprunts souscrits.</p>
+      <p><strong>Il est donc primordial que les emprunteurs disposent des éléments d'analyses qui leur permettent de prendre en compte cet état de fait.</strong></p>
     </div>
   </div>
   <div class="row">
