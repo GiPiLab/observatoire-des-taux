d1dc83f55c81353b9ee23cfb752c6466d540a1ed
Thibault <thibault@gipilab.org>
2018-01-18 19:38:34 +0100

    Update highcharts to 6.0.4

--- server/index.php
+++ server/index.php
@@ -226,8 +226,8 @@ input.highcharts-range-selector {
 <?php }?>
 
 <?php if($pageCourante!=7 && $pageCourante!=12){?>
-<script type="text/javascript" src="https://code.highcharts.com/stock/6.0.3/highstock.js"></script>
-<script type="text/javascript" src="https://code.highcharts.com/stock/6.0.3/highcharts-more.js"></script>
+<script type="text/javascript" src="https://code.highcharts.com/stock/6.0.4/highstock.js"></script>
+<script type="text/javascript" src="https://code.highcharts.com/stock/6.0.4/highcharts-more.js"></script>
 <script type="text/javascript" src="progs/taux/js_thib_pour_taux.js"></script>
 <?php }?>
 
